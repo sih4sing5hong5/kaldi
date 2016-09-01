62feb8f7dde6ff4a3c420eef6a05f9609965ec34
薛丞宏 <ihcaoe@gmail.com>
2016-09-02 07:05:33 +0800

    愛先做辭典再做語言資料

--- egs/taiwanese/s5c/run.sh
+++ egs/taiwanese/s5c/run.sh
@@ -13,6 +13,8 @@ for x in data/train/*; do
 done
 utils/utt2spk_to_spk2utt.pl data/train/utt2spk > data/train/spk2utt
 
+utils/prepare_lang.sh data/local/dict "<UNK>"  data/local/lang data/lang
+
 # # Now train the language models.
 
 # # Compiles G for trigram LM
@@ -30,8 +32,6 @@ cat $LM | \
       --osymbols=data/lang/words.txt  --keep_isymbols=false --keep_osymbols=false | \
      fstrmepsilon > data/lang/G.fst
 
-
-utils/prepare_lang.sh data/local/dict "<UNK>"  data/local/lang data/lang
 # Now make MFCC features.
 # mfccdir should be some place with a largish disk where you
 # want to store MFCC features.
