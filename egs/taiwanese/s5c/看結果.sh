e4838fa5d5bc42b4b96e62207042f157e6960f03
fafoy <ihcaoe@gmail.com>
2016-09-10 16:37:08 +0800

    改用人寫好的script

--- egs/taiwanese/s5c/看結果.sh
+++ egs/taiwanese/s5c/看結果.sh
@@ -9,5 +9,5 @@ set -e # exit on error
 
 for x in tri1 tri2 tri3 tri4 tri4_mmi_b0.1 tri4_fmmi_b0.1; do
   echo "$x:"
-  cat exp/$x/decode_train_dev/wer_* | grep WER | sort -g | head -n 1
+  cat exp/$x/decode_train_dev/wer_* | grep WER | ./utils/best_wer.sh 
 done
