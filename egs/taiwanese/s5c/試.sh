81df77aca82a6f9c0a53384516588ea7c58ebb0c
薛丞宏 <ihcaoe@gmail.com>
2016-09-15 22:00:45 +0800

    lm傷大，愛用rescore的方式

--- egs/taiwanese/s5c/試.sh
+++ egs/taiwanese/s5c/試.sh
@@ -10,7 +10,12 @@ set -e # exit on error
 
 graph_dir=exp/tri4/graph_tshi3
 # # Now train the language models.
-if [ $# -gt 0 ]; then
+if [ $# -eq 1 ]; then
+  LM_GZ='tshi3/lang/語言模型.lm.gz'
+  utils/build_const_arpa_lm.sh $LM_GZ \
+   data/lang tshi3/lang
+fi
+if [ $# -eq 2 ]; then
   utils/prepare_lang.sh tshi3/local/dict "<UNK>"  tshi3/local/lang tshi3/lang
   # # Compiles G for trigram LM
   LM='tshi3/lang/語言模型.lm'
@@ -43,8 +48,22 @@ steps/make_mfcc.sh --nj 1 --cmd "$train_cmd" \
  $tshi3 $make_mfcc_dir $mfccdir
 steps/compute_cmvn_stats.sh $tshi3 $make_mfcc_dir $mfccdir
 
-  (
-    steps/decode_fmllr.sh --nj 1 --cmd "$decode_cmd" \
-      --config conf/decode.config \
-      $graph_dir $tshi3 exp/tri4/decode_tshi3
-  )
+graph_dir=exp/tri4/graph
+(
+steps/decode_fmllr.sh --nj 1 --cmd "$decode_cmd" \
+  --config conf/decode.config \
+  $graph_dir $tshi3 exp/tri4/decode_tshi3
+
+# steps/decode_sgmm2.sh --nj $dev_nj --cmd "$decode_cmd" \
+#   --transform-dir exp/tri3b/decode_dev \
+#   exp/sgmm2_5b2/graph data/dev exp/sgmm2_5b2/decode_dev
+
+steps/lmrescore_const_arpa.sh  --cmd "$decode_cmd" \
+  data/lang tshi3/lang $tshi3 \
+  exp/tri4/decode_tshi3 exp/tri4/decode_tshi3.rescored
+
+# steps/decode_sgmm2.sh --nj $dev_nj --cmd "$decode_cmd" \
+#   --transform-dir exp/tri4/decode_tshi3 \
+#   exp/tri4/graph_big $tshi3 tri4/decode_tshi3.big
+
+)
