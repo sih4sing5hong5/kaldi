e28b2b585feaf66c28b5a22c9c25712c2abda895
薛丞宏 <ihcaoe@gmail.com>
2016-09-16 13:36:19 +0800

    攏加mpe來比較，而且改4个job

--- egs/taiwanese/s5c/試rescore.sh
+++ egs/taiwanese/s5c/試rescore.sh
@@ -12,7 +12,7 @@ LM='tshi3/lang/語言模型.lm'
 
 LANG_DIR='tshi3/lang_rescore'
 
-utils/prepare_lang.sh tshi3/local/dict "<UNK>"  tshi3/local/lang tshi3/lang
+utils/prepare_lang.sh data/local/dict "<UNK>"  tshi3/local/lang tshi3/lang
 
 LM_GZ='tshi3/lang/語言模型.lm.gz'
 cat $LM | gzip > $LM_GZ
@@ -28,14 +28,25 @@ tshi3='tshi3/train'
   make_mfcc_dir=exp/make_mfcc/tshi3
   rm -rf $mfccdir make_mfcc_dir
 
-  steps/make_mfcc.sh --nj 1 --cmd "$train_cmd" \
+  steps/make_mfcc.sh --nj 4 --cmd "$train_cmd" \
    $tshi3 $make_mfcc_dir $mfccdir
   steps/compute_cmvn_stats.sh $tshi3 $make_mfcc_dir $mfccdir
 )
 (
   graph_dir=exp/tri4/graph
 
-  steps/decode_fmllr.sh --nj 1 --cmd "$decode_cmd" \
+  steps/decode_fmllr.sh --nj 4 --cmd "$decode_cmd" \
+    --config conf/decode.config \
+    $graph_dir $tshi3 exp/tri4_mpe/decode_tshi3
+
+  steps/lmrescore_const_arpa.sh  --cmd "$decode_cmd" \
+    data/lang $LANG_DIR $tshi3 \
+    exp/tri4_mpe/decode_tshi3 exp/tri4_mpe/decode_tshi3.rescored
+)
+(
+  graph_dir=exp/tri4/graph
+
+  steps/decode_fmllr.sh --nj 4 --cmd "$decode_cmd" \
     --config conf/decode.config \
     $graph_dir $tshi3 exp/tri4/decode_tshi3
 
