fa5a290d3344da36880ebbf456f2a93f81c92d81
薛丞宏 <ihcaoe@gmail.com>
2016-10-12 19:52:43 +0800

    依錯誤數量決定音檔愛提掉無

--- egs/taiwanese/s5c/揣可能有問題的音檔.sh
+++ egs/taiwanese/s5c/揣可能有問題的音檔.sh
@@ -3,5 +3,14 @@ set -e # exit on error
 grep WARN exp/{tri1,tri2,tri3,tri4}/log/align.* | \
   grep tong | \
   sed 's/.*\(tong.*ku[0-9]*\).*/\1/g' | \
-  sort -u > bo5-ai3.pio2
+  sort | \
+  uniq -c > king3ko3.pio2
+
+if [ $# -eq 0 ]; then
+  cat king3ko3.pio2 | awk '{print $1}' | sort -n | uniq -c | less
+else
+  cat king3ko3.pio2 | \
+    awk -v liong7=$1 '{if($1>=liong7)print $2}' | \
+    tee bo5-ai3.pio2
+fi
 
