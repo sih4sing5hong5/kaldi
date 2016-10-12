d5f758122d4a401e327fcda4057c462c6053d822
薛丞宏 <ihcaoe@gmail.com>
2016-10-12 22:21:54 +0800

    段落名的漢字會造成kaldi程式錯誤，用sed提掉

--- egs/taiwanese/s5c/揣聲音.sh
+++ egs/taiwanese/s5c/揣聲音.sh
@@ -4,6 +4,9 @@ for last; do true; done
 
 grep "$last" data/train/segments
 grep "$last" data/train/text  -A 2 -B 2
-grep "$last" data/train/segments | head -n 1 | tail -n 1 | extract-segments "scp:data/train/wav.scp" "-" "ark:-" >aa 2> /dev/null
+grep "$last" data/train/segments | head -n 1 | tail -n 1 | \
+  sed 's/tong.*ku//g' | \
+  extract-segments "scp:data/train/wav.scp" "-" "ark:-" >aa 2> /dev/null
 sox -r 16000 -e signed -b 16 -c 1 -t raw aa aa.wav
 normalize-audio aa.wav
+play aa.wav 2> /dev/null
