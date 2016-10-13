285fb232ef01c6a7720242f20908640a2e824b70
fafoy <ihcaoe@gmail.com>
2016-10-13 21:13:04 +0800

    聽一寡音檔，修聽的功能會當重聽

--- egs/taiwanese/s5c/聽聲音.sh
+++ egs/taiwanese/s5c/聽聲音.sh
@@ -20,14 +20,18 @@ lai5thiann1=`cat $1 | \
   gshuf -n 100`
 
 for i in $lai5thiann1; do
-	echo bash 揣聲音.sh $i
-  read -p "a好，b無好?" yn
-  if [ $yn = "a" ]; then
-    echo "$i" >> 無問題的音檔.表
-  elif  [ $yn = "b" ]; then
-    echo "$i" >> 有問題的音檔.表
-  else
-    echo '無採用'
-  fi
-  echo
+	while true ; do
+    bash 揣聲音.sh $i
+    read -p "a好，b無好?" yn
+    if [ $yn = "a" ]; then
+      echo "$i" >> 無問題的音檔.表
+      break
+    elif  [ $yn = "b" ]; then
+      echo "$i" >> 有問題的音檔.表
+      break
+    else
+      echo '無採用'
+    fi
+    echo
+  done
 done
