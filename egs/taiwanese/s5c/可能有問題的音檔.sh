cfcd188e0797a7235129033960c2b84a1d5d81a9
fafoy <ihcaoe@gmail.com>
2016-10-15 15:23:46 +0800

    產生無愛表的時，愛先共聽過的音檔提掉

--- egs/taiwanese/s5c/可能有問題的音檔.sh
+++ egs/taiwanese/s5c/可能有問題的音檔.sh
@@ -3,6 +3,8 @@ set -e # exit on error
 grep WARN exp/{mono,tri1,tri2,tri3,tri4}{,_ali}/log/a* | \
   grep tong | \
   sed 's/.*\(tong.*ku[0-9]*\).*/\1/g' | \
+  grep -vwF -f 有問題的音檔.表 | \
+  grep -vwF -f 無問題的音檔.表 | \
   sort | \
   uniq -c > king3ko3.pio2
 
