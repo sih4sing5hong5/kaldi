a0fec82f185ae4aaf2da1e6456d0d7ae8d78b32a
fafoy <ihcaoe@gmail.com>
2016-10-15 08:34:44 +0800

    介面改聲音較好聽

--- egs/taiwanese/s5c/揣聲音.sh
+++ egs/taiwanese/s5c/揣聲音.sh
@@ -4,6 +4,10 @@ for last; do true; done
 
 grep "$last" data/train/segments
 grep "$last" data/train/text  -A 2 -B 2
+echo
+grep "$last" data/train/text | sed 's/^[^ ]* //g'
+echo
+
 grep "$last" data/train/segments | head -n 1 | tail -n 1 | \
   sed 's/tong.*ku//g' | \
   extract-segments "scp:data/train/wav.scp" "-" "ark:-" >aa 2> /dev/null
--- egs/taiwanese/s5c/聽聲音.sh
+++ egs/taiwanese/s5c/聽聲音.sh
@@ -17,20 +17,21 @@ lai5thiann1=`cat $1 | \
   awk '{print $1}' | \
   grep -vwF -f 有問題的音檔.表 | \
   grep -vwF -f 無問題的音檔.表 | \
-  gshuf -n 100`
+  gshuf -n 50`
 
 for i in $lai5thiann1; do
+  bash 揣聲音.sh $i
 	while true ; do
-    bash 揣聲音.sh $i
-    read -p "a好，b無好?" yn
-    if [ $yn = "a" ]; then
+    read -p "e好，f無好?" yn
+    if [ $yn = "e" ]; then
       echo "$i" >> 無問題的音檔.表
       break
-    elif  [ $yn = "b" ]; then
+    elif  [ $yn = "f" ]; then
       echo "$i" >> 有問題的音檔.表
       break
     else
       echo '無採用'
+      play aa.wav 2> /dev/null
     fi
     echo
   done
