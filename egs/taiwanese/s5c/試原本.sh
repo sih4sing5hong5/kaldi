e2dea0fe070ccec5e331e5a2c289c2faf9bede65
薛丞宏 <ihcaoe@gmail.com>
2016-10-19 12:33:01 +0800

    用原本的訓練內試驗的lang來辨識

---
+++ egs/taiwanese/s5c/試原本.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+. cmd.sh
+. path.sh
+
+# This setup was modified from egs/swbd/s5c, with the following changes:
+
+set -e # exit on error
+
+tshi3='tshi3/train'
+(
+  utils/utt2spk_to_spk2utt.pl $tshi3/utt2spk > $tshi3/spk2utt
+
+  utils/fix_data_dir.sh $tshi3
+
+  mfccdir=tshi3/mfcc
+  make_mfcc_dir=exp/make_mfcc/tshi3
+  rm -rf $mfccdir $make_mfcc_dir
+
+  steps/make_mfcc.sh --nj 4 --cmd "$train_cmd" \
+   $tshi3 $make_mfcc_dir $mfccdir
+  steps/compute_cmvn_stats.sh $tshi3 $make_mfcc_dir $mfccdir
+)
+graph_dir=exp/tri4/graph
+(
+  steps/decode_fmllr.sh --nj 1 --cmd "$decode_cmd" \
+    --config conf/decode.config \
+    $graph_dir $tshi3 exp/tri4/decode_tshi3_data_lang
+)
