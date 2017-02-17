cd5c51d9b736b7bdc52d5728f9bb9e7d19d9ad3f
薛丞宏 <ihcaoe@gmail.com>
2017-02-17 14:43:13 +0800

    加服務愛用的script

---
+++ egs/taiwanese/s5c/服務來試.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+. cmd.sh
+. path.sh
+
+# 服務來試.sh exp/model/graph_sp data/train exp/model/decode_hok8bu7_1
+
+set -e # exit on error
+
+tshi3=$2
+(
+  utils/utt2spk_to_spk2utt.pl $tshi3/utt2spk > $tshi3/spk2utt
+
+  utils/fix_data_dir.sh  $tshi3
+
+  mfccdir=$tshi3/mfcc
+  make_mfcc_dir=$tshi3/make_mfcc/
+
+  steps/make_mfcc.sh --nj 1 --cmd "$train_cmd" \
+   $tshi3 $make_mfcc_dir $mfccdir
+  steps/compute_cmvn_stats.sh $tshi3 $make_mfcc_dir $mfccdir
+)
+graph_dir=$1
+decode_dir=$3
+(
+  steps/decode_fmllr.sh --nj 1 --cmd "$decode_cmd" \
+    --config conf/decode.config \
+    $graph_dir $tshi3 $decode_dir
+)
