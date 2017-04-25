4fd68ea52712211ac28320338b705c0344001174
薛丞宏 <ihcaoe@gmail.com>
2017-04-25 21:26:54 +0800

    對齊了，吐出詞的ctm

--- egs/taiwanese/s5c/對齊音檔.sh
+++ egs/taiwanese/s5c/對齊音檔.sh
@@ -9,11 +9,11 @@ set -e # exit on error
 
 tshi3=$2
 tshi3='tui3tse5/train'
-lang='data/lang_sp'
+lang='tui3tse5/lang_dict'
 (
   utils/utt2spk_to_spk2utt.pl $tshi3/utt2spk > $tshi3/spk2utt
 
-  utils/fix_data_dir.sh  $tshi3
+  utils/fix_data_dir.sh $tshi3
 
   mfccdir=$tshi3/mfcc
   make_mfcc_dir=$tshi3/make_mfcc/
@@ -23,19 +23,10 @@ lang='data/lang_sp'
   steps/compute_cmvn_stats.sh $tshi3 $make_mfcc_dir $mfccdir
 )
 
-steps/align_fmllr.sh --nj 1 --cmd "$train_cmd" \
+cp data/local/dict/[^l]* tui3tse5/local/dict
+utils/prepare_lang.sh tui3tse5/local/dict "<UNK>"  tui3tse5/local/lang $lang
+
+steps/align_fmllr.sh --beam 100 --retry-beam 150 --nj 1 --cmd "$train_cmd" \
   $tshi3 $lang exp/tri5.2 exp/tui3tse5_ali
-
-# zcat exp/tui3tse5_ali/ali.1.gz | \
-#   lattice-push ark:- ark:- | \
-#   lattice-align-words data/lang_sp/phones/word_boundary.int exp/tri5.2/final.mdl ark:- ark:- | \
-#   lattice-to-ctm-conf --acoustic-scale=0.1 ark:- exp/lang_sp/1.conf
-
-
-zcat exp/tui3tse5_ali/ali.1.gz | \
-  ali-to-post ark:- ark,t:exp/tui3tse5_ali/1.conf
-
-zcat exp/tui3tse5_ali/ali.1.gz | \
-  ali-to-phones --ctm-output --ctm-output exp/tri5.2/final.mdl ark:- exp/tui3tse5_ali/1.ctm
 
 steps/get_train_ctm.sh $tshi3 $lang exp/tui3tse5_ali
