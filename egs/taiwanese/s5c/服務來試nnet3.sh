169b15a77ea3eca7804cf40aef23cf6ea798b71c
薛丞宏 <ihcaoe@gmail.com>
2017-11-02 17:40:27 +0800

    先用ricer的指令佮參數

--- egs/taiwanese/s5c/服務來試nnet3.sh
+++ egs/taiwanese/s5c/服務來試nnet3.sh
@@ -8,20 +8,21 @@
 set -e # exit on error
 
 tshi3=$2
-# (
-#   utils/utt2spk_to_spk2utt.pl $tshi3/utt2spk > $tshi3/spk2utt
+(
+  utils/utt2spk_to_spk2utt.pl $tshi3/utt2spk > $tshi3/spk2utt
 
-#   utils/fix_data_dir.sh  $tshi3
+  utils/fix_data_dir.sh  $tshi3
 
-#   mfccdir=$tshi3/mfcc
-#   make_mfcc_dir=$tshi3/make_mfcc/
+  mfccdir=$tshi3/mfcc
+  make_mfcc_dir=$tshi3/make_mfcc/
 
-#   steps/make_mfcc.sh --nj 1 --cmd "$train_cmd" \
-#    $tshi3 $make_mfcc_dir $mfccdir
-#   steps/compute_cmvn_stats.sh $tshi3 $make_mfcc_dir $mfccdir
-# )
+  steps/make_mfcc.sh --nj 1 --cmd "$train_cmd" \
+   $tshi3 $make_mfcc_dir $mfccdir
+  steps/compute_cmvn_stats.sh $tshi3 $make_mfcc_dir $mfccdir
+)
 graph_dir=$1
 decode_dir=$3
+mkdir -p $3
 (
   nnet3-latgen-faster \
   --frame-subsampling-factor=3 \
@@ -38,5 +39,20 @@ decode_dir=$3
   exp/nnet3_chain/final.mdl \
   exp/nnet3_chain/HCLG.fst \
   "ark,s,cs:apply-cmvn --norm-means=false --norm-vars=false --utt2spk=ark:s5c-twisas/utt2spk scp:s5c-twisas/cmvn.scp scp:s5c-twisas/feats.scp ark:- |" \
-  "ark:|lattice-scale --acoustic-scale=10.0 ark:- ark:- | gzip -c >exp/nnet3_chain/lat.1.gz" 
+  "ark:|lattice-scale --acoustic-scale=10.0 ark:- ark:- | gzip -c >exp/nnet3_chain/lat.1.gz" 2> $decode_dir/a.log
 )
+
+# (
+#   steps/nnet3/decode.sh --nj 1 --cmd "$decode_cmd" \
+#     --acwt 1.0 \
+#     --post_decode_acwt 10.0 \
+#     --skip_diagnostics true \
+#     --stage 3 \
+#     --scoring_opts '--min_lmwt 1 --max_lmwt 20' \
+#     --config conf/decode.config \
+#     $graph_dir $tshi3 $decode_dir
+# )
+
+# lattice-best-path --word-symbol-table=$graph_dir/words.txt \
+#   "ark:gunzip -c exp/nnet3_chain/lat.1.gz|" ark,t:- \
+#   | utils/int2sym.pl -f 2- $graph_dir/words.txt
