8d7e350ed80b45c190304919302d1b30bcb62818
薛丞宏 <ihcaoe@gmail.com>
2017-11-02 22:23:03 +0800

    ricer講愛用best-path

--- egs/taiwanese/s5c/服務來試nnet3.sh
+++ egs/taiwanese/s5c/服務來試nnet3.sh
@@ -22,7 +22,8 @@ tshi3=$2
 )
 graph_dir=$1
 decode_dir=$3
-mkdir -p $3
+# mkdir -p $3
+mkdir -p $decode_dir/scoring/
 (
   nnet3-latgen-faster \
   --frame-subsampling-factor=3 \
@@ -38,21 +39,23 @@ mkdir -p $3
   --word-symbol-table=exp/nnet3_chain/words.txt \
   exp/nnet3_chain/final.mdl \
   exp/nnet3_chain/HCLG.fst \
-  "ark,s,cs:apply-cmvn --norm-means=false --norm-vars=false --utt2spk=ark:s5c-twisas/utt2spk scp:s5c-twisas/cmvn.scp scp:s5c-twisas/feats.scp ark:- |" \
-  "ark:|lattice-scale --acoustic-scale=10.0 ark:- ark:- | gzip -c >exp/nnet3_chain/lat.1.gz" 2> $decode_dir/a.log
+  "ark,s,cs:apply-cmvn --norm-means=false --norm-vars=false --utt2spk=ark:$tshi3/utt2spk scp:$tshi3/cmvn.scp scp:$tshi3/feats.scp ark:- |" \
+  "ark:|lattice-scale --acoustic-scale=10.0 ark:- ark:- | gzip -c >exp/nnet3_chain/lat.1.gz" 2>&1 | tee $decode_dir/a.log
+  # cat $decode_dir/a.log | grep ^0 > $decode_dir/scoring/7.0.0.txt
 )
 
-# (
-#   steps/nnet3/decode.sh --nj 1 --cmd "$decode_cmd" \
-#     --acwt 1.0 \
-#     --post_decode_acwt 10.0 \
-#     --skip_diagnostics true \
-#     --stage 3 \
-#     --scoring_opts '--min_lmwt 1 --max_lmwt 20' \
-#     --config conf/decode.config \
-#     $graph_dir $tshi3 $decode_dir
-# )
+(
+  steps/nnet3/decode.sh --nj 1 --cmd "$decode_cmd" \
+    --acwt 1.0 \
+    --post_decode_acwt 10.0 \
+    --skip_diagnostics true \
+    --stage 3 \
+    --scoring_opts '--min_lmwt 6 --max_lmwt 20' \
+    --config conf/decode.config \
+    $graph_dir $tshi3 $decode_dir
+)
 
-# lattice-best-path --word-symbol-table=$graph_dir/words.txt \
-#   "ark:gunzip -c exp/nnet3_chain/lat.1.gz|" ark,t:- \
-#   | utils/int2sym.pl -f 2- $graph_dir/words.txt
+lattice-best-path --word-symbol-table=$graph_dir/words.txt \
+  "ark:gunzip -c exp/nnet3_chain/lat.1.gz|" ark,t:- \
+  | utils/int2sym.pl -f 2- $graph_dir/words.txt \
+  | tee $decode_dir/scoring/7.0.0.txt
