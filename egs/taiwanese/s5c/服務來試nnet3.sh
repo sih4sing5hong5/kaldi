a66e117fe2a25d1bf14f7c9e645c718438bb107b
薛丞宏 <ihcaoe@gmail.com>
2018-09-19 15:37:25 +0800

    Decode si ai ivector.

--- egs/taiwanese/s5c/服務來試nnet3.sh
+++ egs/taiwanese/s5c/服務來試nnet3.sh
@@ -13,25 +13,26 @@ tshi3=$3
 (
   utils/utt2spk_to_spk2utt.pl $tshi3/utt2spk > $tshi3/spk2utt
 
-  utils/fix_data_dir.sh $tshi3
-
-  mfccdir=$tshi3/mfcc
-  make_mfcc_log=$tshi3/make_mfcc/
+  
+  mfccdir=${tshi3}_hires/mfcc
+  make_mfcc_log=${tshi3}_hires/make_mfcc/
 
 
     utils/copy_data_dir.sh $tshi3 ${tshi3}_hires
+  
+    utils/fix_data_dir.sh ${tshi3}_hires
 
     steps/make_mfcc_pitch.sh --nj $nj --mfcc-config conf/mfcc_hires.conf \
       --cmd "$train_cmd" ${tshi3}_hires $make_mfcc_log $mfccdir
     steps/compute_cmvn_stats.sh ${tshi3}_hires $make_mfcc_log $mfccdir 
 
-    utils/fix_data_dir.sh ${tshi3}_hires
     # create MFCC data dir without pitch to extract iVector
     utils/data/limit_feature_dim.sh 0:39 ${tshi3}_hires ${tshi3}_hires_nopitch 
     steps/compute_cmvn_stats.sh ${tshi3}_hires_nopitch $make_mfcc_log $mfccdir
+  online_ivector_dir=exp/nnet3/ivectors_test
     steps/online/nnet2/extract_ivectors_online.sh --cmd "$train_cmd" --nj $nj \
       ${tshi3}_hires_nopitch exp/nnet3/extractor \
-      exp/nnet3/ivectors_test
+      $online_ivector_dir
 )
 graph_dir=$1
 lang_dir=$2
@@ -40,6 +41,8 @@ decode_dir=$4
 # mkdir -p $3
 mkdir -p $decode_dir/scoring/
 (
+  ivector_period=$(cat $online_ivector_dir/ivector_period) || exit 1;
+  ivector_opts="--online-ivectors=scp:$online_ivector_dir/ivector_online.scp --online-ivector-period=$ivector_period"
   nnet3-latgen-faster \
   --frame-subsampling-factor=3 \
   --frames-per-chunk=51 \
@@ -52,7 +55,8 @@ mkdir -p $decode_dir/scoring/
   --beam=15.0 --lattice-beam=8.0 --acoustic-scale=1.0 \
   --allow-partial=true \
   --word-symbol-table=$graph_dir/words.txt \
-  exp/nnet3_chain/final.mdl \
+  ivector_opts \
+  $graph_dir/../final.mdl \
   $graph_dir/HCLG.fst \
   "ark,s,cs:apply-cmvn --norm-means=false --norm-vars=false --utt2spk=ark:${tshi3}_hires/utt2spk scp:${tshi3}_hires/cmvn.scp scp:${tshi3}_hires/feats.scp ark:- |" \
   "ark:|lattice-scale --acoustic-scale=10.0 ark:- ark:- | gzip -c > $decode_dir/lat1.1.gz" 2>&1 | tee $decode_dir/a.log
